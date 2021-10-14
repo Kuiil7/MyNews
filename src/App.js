a3a71a929b7aeb04ed7c0813c0d72b088059a228
kuiil7 <kuiil7ig11@gmail.com>
2021-10-14 00:13:24 -0700

    updated code w async and added props component

--- src/App.js
+++ src/App.js
@@ -1,157 +1,100 @@
+
 import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
-import { DateTime } from 'luxon';
-
-import './App.css'
+import Header from './Header'
+import Content from './Content'
 
 require('dotenv').config()
 
-function App() {
+const AxiosDemo = (props) => {
 
-  const [data, setData] = useState({ articles: [] });
-
-  const [isError, setIsError] = useState(false);
+  const [articleData, setArticleData] = useState({articles:[]});
 
   const [query, setQuery] = useState('');
 
+  const [url, setUrl] = useState(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_API_KEY}`);
+
   const [isLoading, setIsLoading] = useState(false);
-
-
-
-  const [url, setUrl] = useState(
-    `  https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_API_KEY}`
-
-  );
-
+  const [isError, setIsError] = useState(false);
 
 
   useEffect(() => {
+
     const fetchData = async () => {
 
       setIsLoading(true);
-
-      setIsError(false);
+      setIsError(false)
 
       const result = await axios(url);
-
-      setData(result.data);
+      setArticleData(result.data);
       setIsLoading(false);
       console.log(result.data)
-
     };
-
     fetchData();
   }, [url]);
-
-
-
-
-
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
-    <Fragment>
-
-<section className="hero is-success is-small">
-  <div className="hero-body">
-    <p className="title">
-    Welcome to MyNews!
-    </p>
-    <p className="subtitle is-7 is-italic">
-     Your #1 World News search site powered by Newsapi.org!
-     <br/>
-     Search every article published by over 80,000 different sources large and small in the last 3 years.
-
-    </p>
-
-    <p className="is-pulled-right">
-    Today's date: {DateTime.now().toFormat('LLLL dd yyyy')}
-
-    </p>
-
-  </div>
-</section>
+    <div>
 
 
 
-<form onSubmit={event => {
-        setUrl(`https://newsapi.org/v2/everything?q=${query}&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`);
+<Header />
 
+      <form onSubmit={event => {
+        setUrl(`https://newsapi.org/v2/everything?q=${query}&apiKey=${process.env.REACT_APP_API_KEY}`);
         event.preventDefault();
       }}>
 
 
-  <div className="column is-6  is-half is-offset-one-quarter">
-
+<div class="columns is-justify-content-center ">
+  <div className="column
+is-5
+mt-3
+">
   <input
           type="text"
           value={query}
           onChange={event => setQuery(event.target.value)}
-          className="input is-primary mb-2"
-          placeholder="enter a keyword"
-
+          className="input is-primary mb-2  is-rounded"
+          placeholder="enter a city name"
         />
-   <button className="button is-small is-primary" type="submit">Search</button>
-
+<button className="button is-small is-primary  is-rounded" type="submit">Search</button>
   </div>
-
-
-
-
+  </div>
  </form>
 
-      {isError && <div>Something went wrong ...</div>}
-
-      {isLoading ? (
-        <div>Loading Latest Top Headlines...</div>
-      ) : (
-        <div className="container ">
-<div className="columns is-flex-wrap-wrap is-centered">
 
 
-{data.articles && data.articles.map((article,i) => (
+<div className="columns is-flex-wrap-wrap is-justify-content-center  "   >
+      {articleData.articles && articleData.articles.map(article => (
 
-  <div className="column is-one-quarter box m-1  " key={i} >
-
-
-      <div className='reverse-columns is-link is-small'>
-
-      <a href={article.url}>{article.title}</a>
-
-      <img src={article.urlToImage} alt="article urls"></img>
-
-      <p className="is-italic subtitle is-7">
- {article.source.name}.
- <br/>
-         Last Updated: {DateTime.fromISO(article.publishedAt).toFormat('LLLL dd yyyy')}
-        </p>
-
-        </div>
-    </div>
+<div className="column is-one-quarter box m-1  " key={article} >
 
 
 
 
- ))}
+<Content
+url={article.url}
+name={article.source.name}
+urlToImage={article.urlToImage}
+publishedAt={article.publishedAt}
+title={article.title}content={article.content}
+description={article.description}
 
+/>
 
-
-</div>
-        </div>
-      )}
-
-
-<div >
-
-  <div className="content has-text-centered mt-6 has-background-primary" style={{height:"60px"}}>
-    <p className="has-text-white m-7 is-justify-content-center">
-      MyNews by <a className="has-text-white " href="https://jgamworks.com">jgamworks.com</a>.
-    </p>
   </div>
 
-</div>
 
 
-    </Fragment>
+
+))}
+ </div>
+
+      {isError && <div>Error fetching data.</div>}
+    </div>
   );
-}
-
-export default App;
+};
+export default AxiosDemo;
